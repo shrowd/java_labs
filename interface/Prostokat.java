2d5e570a9f02f0e394b9f7fce27ce6e914480812
shrowd <118308030+shrowd@users.noreply.github.com>
2022-11-17 20:08:53 +0100

    Update Prostokat.java

--- interface/Prostokat.java
+++ interface/Prostokat.java
@@ -14,9 +14,4 @@ public class Prostokat implements Figura {
     public String toString() {
         return "Prostokat: " + a + " x " + b;
     }
-    public static void main(String[] args){
-        Prostokat p1 = new Prostokat(1,2);
-        System.out.println(p1.obwod());
-        System.out.println(p1.pole());
-    }
 }
