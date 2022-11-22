b8604add5ac3c354fd4688c79f2bccf0f2c29b9a
shrowd <118308030+shrowd@users.noreply.github.com>
2022-11-22 16:35:32 +0100

    zadania

---
+++ lab 5 - zadania/zadanie 1/Punkt.java
@@ -0,0 +1,11 @@
+class Punkt
+{
+    double x,y;
+    Punkt(double x,double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public String toString(){
+        return "Punkt (" + x + "," + y + ")";
+    }
+}
